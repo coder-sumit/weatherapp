92720eb0a313032b3aaa84f936f1c74146295ea8
Sumit Baghel <studyzone.sumit@gmail.com>
2022-10-24 14:00:06 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port||8000;
+const PORT = process.env.PORT||8000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
